8ff90cb623b4de82087bfdee07ce073e35d9b95b
zaxwebs <zifidigital@gmail.com>
2020-06-04 12:07:33 +0530

    Added variable add, subtract and sets

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import './App.css'
 
 function App() {
   const count = useStoreState((state) => state.counter.count)
+  const delta = useStoreState((state) => state.counter.delta)
   const increment = useStoreActions((actions) => actions.counter.increment)
   const decrement = useStoreActions((actions) => actions.counter.decrement)
+  const reset = useStoreActions((actions) => actions.counter.reset)
+  const set = useStoreActions((actions) => actions.counter.set)
+  const updateDelta = useStoreActions((actions) => actions.counter.updateDelta)
 
   return (
     <div className="app">
       <div className="counter">
         <div className="count">{count}</div>
         <div className="controller">
-          <button onClick={() => increment()}>Add</button>
-          <button onClick={() => decrement()}>Subtract</button>
+          <input
+            type="number"
+            onChange={(e) => updateDelta(e.target.value)}
+            value={delta}
+          ></input>
+          <div className="buttons">
+            <button onClick={() => increment()}>Add</button>
+            <button onClick={() => decrement()}>Subtract</button>
+            <button onClick={() => set()}>Set</button>
+            <button onClick={() => reset()}>Reset</button>
+          </div>
         </div>
       </div>
     </div>
--- src/models/counterModel.js
+++ src/models/counterModel.js
@@ -2,10 +2,20 @@ import { action } from 'easy-peasy'
 
 export default {
   count: 0,
-  increment: action((state, payload = 1) => {
-    state.count = state.count + payload
+  delta: 1,
+  increment: action((state) => {
+    state.count = state.count + state.delta
   }),
-  decrement: action((state, payload = 1) => {
-    state.count = state.count - payload
+  decrement: action((state) => {
+    state.count = state.count - state.delta
+  }),
+  reset: action((state) => {
+    state.count = 0
+  }),
+  set: action((state, payload = 0) => {
+    state.count = state.delta
+  }),
+  updateDelta: action((state, payload = 1) => {
+    state.delta = parseFloat(payload)
   }),
 }
